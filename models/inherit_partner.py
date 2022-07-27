30af351e4127875c8ff3b258a956dbc12cc80166
Santiago Ramos Morales <sramos@estrasol.com.mx>
2022-07-27 18:42:56 -0500

    Fixing logic for Odoo v11 IV

--- models/inherit_partner.py
+++ models/inherit_partner.py
@@ -8,13 +8,13 @@ class InheritedPartner(models.Model):
     credit_enabled = fields.Boolean(
         string='¿Aplica crédito disponible?', default=False)
     available_credit = fields.Float(
-        string='Crédito Disponible', compute_sudo=True, compute='_compute_available_credit', store=True)
+        string='Crédito Disponible', compute='_compute_available_credit')
 
-    @api.depends('sale_order_count', 'credit_limit', 'credit_enabled', 'sale_order_ids')
+    
     def _compute_available_credit(self):
         for record in self:
             available_credit = record.credit_limit - record.credit
-            record.available_credit = available_credit
             for sale_order in record.sale_order_ids:
-                if sale_order.invoice_status == 'to invoice' and sale_order.state not in ['cancel', 'draft'] and sale_order.payment_term_id.id != 1:
-                    record.available_credit -= sale_order.amount_total
+                if sale_order.invoice_status == 'to invoice' and sale_order.state not in ['cancel', 'draft']:
+                    available_credit -= sale_order.amount_total
+            record.available_credit = available_credit
--- models/inherit_sale_order.py
+++ models/inherit_sale_order.py
@@ -1,22 +1,27 @@
 from odoo import models, fields, api
 import logging
+
 _logger = logging.getLogger(__name__)
 
+
 class InheritedSaleOrder(models.Model):
+
     _inherit = 'sale.order'
 
     partner_has_credit = fields.Boolean(
         related='partner_id.credit_enabled', readonly=True)
     available_credit = fields.Float(
-        string='Crédito Dispoible', compute='_compute_available_credit', readonly=True, copy=False, store=True)
+        string='Crédito Dispoible', compute='_compute_available_credit_so', readonly=True, copy=False, store=True)
     credit_after_sale = fields.Float(
         string='Crédito después de la venta', compute='_compute_credit_after_sale', store=True)
 
+
     @api.depends('partner_id')
-    def _compute_available_credit(self):
+    def _compute_available_credit_so(self):
         for record in self:
             record.available_credit = record.partner_id.available_credit
             _logger.info(str(record.available_credit))
+
 
     @api.depends('amount_total', 'order_line.price_total')
     def _compute_credit_after_sale(self):
@@ -26,20 +31,24 @@ class InheritedSaleOrder(models.Model):
                 amount += order.price_total
         record.credit_after_sale = record.available_credit - amount
 
+
+    @api.model
     def write(self, vals):
         if self._context.get('skip'):
-            return super(InheritedSaleOrder, self).write(vals)
+            super(InheritedSaleOrder, self).write(vals)
         if self.state == 'draft' and self.credit_after_sale > 0 and self.partner_has_credit:
             super(InheritedSaleOrder, self).write(vals)
             self.with_context(skip=True).action_confirm()
         else:
-            return super(InheritedSaleOrder, self).write(vals)
+            super(InheritedSaleOrder, self).write(vals)
+        self.partner_id._compute_available_credit()
 
     @api.model
     def create(self, vals):
         res = super(InheritedSaleOrder, self).create(vals)
         partner = self.env['res.partner'].search(
             [('id', '=', vals['partner_id'])])
-        if partner.credit_enabled and partner.available_credit - self.credit_after_sale > 0:
+        if partner.credit_enabled and partner.available_credi > 0:
             res.with_context(skip=False).action_confirm()
+        self.partner_id._compute_available_credit()
         return res
