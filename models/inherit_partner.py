dd31a50f589e6fb2adb143fbb35e8abc7f90d61c
Santiago Ramos Morales <sramos@estrasol.com.mx>
2022-07-27 14:14:32 -0500

    Fixing logic for Odoo v11 II

--- models/inherit_partner.py
+++ models/inherit_partner.py
@@ -8,10 +8,10 @@ class InheritedPartner(models.Model):
     credit_enabled = fields.Boolean(string='¿Aplica crédito disponible?', default=False)
     available_credit = fields.Float(string='Crédito Disponible', compute='_compute_available_credit', store=True)
 
-    @api.depends('sale_order_ids', 'credit_limit')
+    @api.depends('sale_order_ids', 'credit_limit', 'credit_enabled')
     def _compute_available_credit(self):
         for record in self:
             record.available_credit = record.credit_limit - record.credit
             for sale_order in record.sale_order_ids:
-                if sale_order.invoice_status == 'to invoice':
+                if sale_order.invoice_status == 'to invoice' and sale_order.payment_term_id.id not in [1, False]:
                     record.available_credit -= sale_order.amount_total
