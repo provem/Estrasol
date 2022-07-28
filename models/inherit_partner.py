cac42474042f17270c178502b24412b17e6c3e2e
Santiago Ramos Morales <sramos@estrasol.com.mx>
2022-07-28 09:55:03 -0500

    Fixing logic for Odoo v11 V

--- models/inherit_partner.py
+++ models/inherit_partner.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class InheritedPartner(models.Model):
 
@@ -8,13 +9,15 @@ class InheritedPartner(models.Model):
     credit_enabled = fields.Boolean(
         string='¿Aplica crédito disponible?', default=False)
     available_credit = fields.Float(
-        string='Crédito Disponible', compute='_compute_available_credit')
+        string='Crédito Disponible', compute='_compute_available_credit', store=True, readonly=True)
 
     
     def _compute_available_credit(self):
         for record in self:
             available_credit = record.credit_limit - record.credit
+            _logger.info(str(available_credit))
             for sale_order in record.sale_order_ids:
                 if sale_order.invoice_status == 'to invoice' and sale_order.state not in ['cancel', 'draft']:
                     available_credit -= sale_order.amount_total
             record.available_credit = available_credit
+            _logger.info(str(record.available_credit))
--- models/inherit_sale_order.py
+++ models/inherit_sale_order.py
@@ -32,23 +32,22 @@ class InheritedSaleOrder(models.Model):
         record.credit_after_sale = record.available_credit - amount
 
 
-    @api.model
-    def write(self, vals):
-        if self._context.get('skip'):
-            super(InheritedSaleOrder, self).write(vals)
-        if self.state == 'draft' and self.credit_after_sale > 0 and self.partner_has_credit:
-            super(InheritedSaleOrder, self).write(vals)
-            self.with_context(skip=True).action_confirm()
-        else:
-            super(InheritedSaleOrder, self).write(vals)
-        self.partner_id._compute_available_credit()
+    # @api.model
+    # def write(self, vals):
+    #     if self._context.get('skip'):
+    #         super(InheritedSaleOrder, self).write(vals)
+    #     if self.state == 'draft' and self.credit_after_sale > 0 and self.partner_has_credit:
+    #         super(InheritedSaleOrder, self).write(vals)
+    #         self.with_context(skip=True).action_confirm()
+    #     else:
+    #         super(InheritedSaleOrder, self).write(vals)
+    #     self.partner_id._compute_available_credit()
 
-    @api.model
-    def create(self, vals):
-        res = super(InheritedSaleOrder, self).create(vals)
-        partner = self.env['res.partner'].search(
-            [('id', '=', vals['partner_id'])])
-        if partner.credit_enabled and partner.available_credi > 0:
-            res.with_context(skip=False).action_confirm()
-        self.partner_id._compute_available_credit()
-        return res
+    # @api.model
+    # def create(self, vals):
+    #     res = super(InheritedSaleOrder, self).create(vals)
+    #     partner = self.env['res.partner'].search([('id', '=', vals['partner_id'])])
+    #     if partner.credit_enabled and partner.available_credi > 0:
+    #         res.with_context(skip=False).action_confirm()
+    #     self.partner_id._compute_available_credit()
+    #     return res
