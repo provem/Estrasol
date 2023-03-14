969cd6a4fe3e1ff32dce23739441918affbf4cd6
Santiago Ramos Morales <sramos@estrasol.com.mx>
2023-03-14 14:32:54 -0600

    Fix

--- credit_limits/models/inherit_partner.py
+++ credit_limits/models/inherit_partner.py
@@ -18,9 +18,15 @@ class InheritedPartner(models.Model):
             for sale_order in record.sale_order_ids:
                 if sale_order.invoice_status == 'to invoice' and sale_order.state == 'sale':
                     used_credit = used_credit + sale_order.amount_total
+                    _logger.info('sale_order.amount_total')
+                    _logger.info('-----------------------')
+                    _logger.info(sale_order.invoice_status)
+                    _logger.info(str(sale_order.id))
+                    _logger.info(str(sale_order.amount_total))
+                    _logger.info('-----------------------')
             record.provem_available_credit = round(record.credit_limit - used_credit)
 
     def get_provem_available_credit(self):
         for record in self:
-            _logger.info(str(record.provem_available_credit))
+            _logger.info(str(len(record.sale_order_ids)))
             return record.provem_available_credit
