af6a027d864ff23efb8681337b0b9b6cc99e78a0
Santiago Ramos Morales <sramos@estrasol.com.mx>
2023-03-14 13:24:42 -0600

    Test

--- credit_limits/models/inherit_partner.py
+++ credit_limits/models/inherit_partner.py
@@ -14,11 +14,11 @@ class InheritedPartner(models.Model):
     @api.depends('sale_order_ids', 'sale_order_ids.state', 'credit_enabled', 'credit_limit')
     def _compute_available_credit(self):
         for record in self:
-            available_credit = 0.0
+            used_credit = 0.0
             for sale_order in record.sale_order_ids:
                 if sale_order.invoice_status == 'to invoice' and sale_order.state not in ['cancel', 'draft']:
-                    available_credit = available_credit - sale_order.amount_total
-            record.available_credit = round(available_credit)
+                    used_credit = used_credit + sale_order.amount_total
+            record.available_credit = round(record.credit_limit - used_credit)
 
     def get_available_credit(self):
         for record in self:
